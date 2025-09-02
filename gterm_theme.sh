45798b3ae0da02f50cc1f3a967fd57bc7e20e9e8
lab1702 <lab1702@gmail.com>
2025-09-01 23:31:53 -0400

    terminal theme tweaks

--- gterm_theme.sh
+++ gterm_theme.sh
@@ -11,18 +11,19 @@ PROFILE=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d "'")
 # Base path for terminal profile settings
 PROFILE_PATH="org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$PROFILE/"
 
+# First reset
+echo "Resetting all settings..."
+gsettings reset-recursively $PROFILE_PATH
+
 # Set dark theme
 echo "Setting dark theme..."
 gsettings set $PROFILE_PATH use-theme-colors false
 gsettings set $PROFILE_PATH use-theme-transparency false
 
 # Configure dark background and foreground colors
-echo "Configuring colors..."
+# echo "Configuring colors..."
 gsettings set $PROFILE_PATH background-color 'rgb(23,20,33)'
 gsettings set $PROFILE_PATH foreground-color 'rgb(208,207,204)'
-
-# Set color palette (Gnome dark colors)
-gsettings set $PROFILE_PATH palette "['rgb(23,20,33)', 'rgb(192,28,40)', 'rgb(38,162,105)', 'rgb(162,115,76)', 'rgb(18,72,139)', 'rgb(163,71,186)', 'rgb(42,161,179)', 'rgb(208,207,204)', 'rgb(94,92,100)', 'rgb(246,97,81)', 'rgb(51,218,122)', 'rgb(233,173,12)', 'rgb(42,123,222)', 'rgb(192,97,203)', 'rgb(51,199,222)', 'rgb(255,255,255)']"
 
 # Enable custom colors
 gsettings set $PROFILE_PATH use-system-font false
