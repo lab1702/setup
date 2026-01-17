5dde2a56f887831869f7372b78042362ded0919e
Lars Bernhardsson <lab1702@gmail.com>
2026-01-16 22:10:52 -0500

    Remove unnecessary gsettings configurations
    Removed several gsettings configurations related to screen lock, location services, time format, weekday display, night light, and desktop folder visibility.

--- ubuntu-gnome-setup.sh
+++ ubuntu-gnome-setup.sh
@@ -7,24 +7,3 @@ gsettings set org.gnome.desktop.background picture-uri ''
 gsettings set org.gnome.desktop.background picture-uri-dark ''
 gsettings set org.gnome.desktop.background primary-color '#000000'
 gsettings set org.gnome.desktop.background color-shading-type 'solid'
-
-# Turn off automatic screen lock
-gsettings set org.gnome.desktop.screensaver lock-enabled false
-
-# Turn on location services
-gsettings set org.gnome.system.location enabled true
-
-# Use AM/PM time format (12-hour clock)
-gsettings set org.gnome.desktop.interface clock-format '12h'
-
-# Show weekday in top bar
-gsettings set org.gnome.desktop.interface clock-show-weekday true
-
-# Turn on night light between 6pm and 6am
-gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
-gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic false
-gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 18.0
-gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to 6.0
-
-# Turn off show home folder on desktop
-gsettings set org.gnome.shell.extensions.ding show-home false
