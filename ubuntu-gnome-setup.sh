4956eecc900098ac9284656efe62589c70f544c7
Lars Bernhardsson <lab1702@gmail.com>
2026-01-25 18:46:04 -0500

    Delete ubuntu-gnome-setup.sh

--- ubuntu-gnome-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ubuntu GNOME Setup Script
-
-# Set background to solid black
-gsettings set org.gnome.desktop.background picture-uri ''
-gsettings set org.gnome.desktop.background picture-uri-dark ''
-gsettings set org.gnome.desktop.background primary-color '#000000'
-gsettings set org.gnome.desktop.background color-shading-type 'solid'
